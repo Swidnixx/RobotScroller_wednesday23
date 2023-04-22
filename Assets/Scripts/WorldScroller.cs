ef0d26a0a08f45189a751acdbb94b09a0fba3ce2
Swidnixx <swidnixx@gmail.com>
2023-04-22 11:38:26 +0200

    WorldScroller now works by looping 2 tiles forever. We also created gameManager as singleton and started building UI

--- Assets/Scripts/WorldScroller.cs
+++ Assets/Scripts/WorldScroller.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class WorldScroller : MonoBehaviour
 {
-    public Transform activeTile1, activeTile2;
+    public SpriteRenderer activeTile1, activeTile2;
     public float speed = 0.1f;
 
     private void FixedUpdate()
     {
-        activeTile1.position += new Vector3( -speed, 0, 0);
-        activeTile2.position += new Vector3( -speed, 0, 0);
+        activeTile1.transform.position += new Vector3( -speed, 0, 0);
+        activeTile2.transform.position += new Vector3( -speed, 0, 0);
+
+        if( activeTile2.transform.position.x < 0 )
+        {
+            activeTile1.transform.position = activeTile2.transform.position +
+                new Vector3( activeTile1.bounds.size.x, 0, 0);
+
+            var tmp = activeTile1;
+            activeTile1 = activeTile2;
+            activeTile2 = tmp;
+        }
     }
 
 }
